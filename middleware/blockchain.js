86063ea50cd82f93d3ab1597df3fded18261c8a4
Thomas_Johnson <tjohnson1106@gmail.com>
2018-02-22 11:45:01 -0500

    added transaction with objects to blockchain file

--- middleware/blockchain.js
+++ middleware/blockchain.js
@@ -1,22 +1,47 @@
- class Blockchain {
-     constructor () {
-         //creating chain and transaction
-         this.chain = []
-         this.current_transactions = []
-         //Binding
-         this.newBlock = this.newBlock.bind(this)
-         this.newTransaction = this.newTransaction.bind(this)
-         this.lastBlock = this.lastBlock.bind(this)
-         this.proofOfWork = this.proofOfWork.bind(this)
-     }
+class Blockchain {
+  constructor() {
+    //creating chain and transaction
+    this.chain = [];
+    this.current_transactions = [];
+    //Binding
+    this.newBlock = this.newBlock.bind(this);
+    this.newTransaction = this.newTransaction.bind(this);
+    this.lastBlock = this.lastBlock.bind(this);
+    this.proofOfWork = this.proofOfWork.bind(this);
+  }
+  newBlock(proof, previousHash) {
+    const Block = {
+      index: this.chain.length + 1,
+      timestamp: new Date(),
+      transactions: this.current_transactions,
+      proof: proof,
+      previous_hash: previousHash
+    };
+    this.current_transactions = [];
+    this.chain.push(block);
+    return block;
+  }
 
-     newBlock ()
+  newTransaction(sender, recipient, amount) {
+    this.current_transactions.push({
+      sender: sender,
+      recipient: recipient,
+      amount: amount
+    });
+    return this.lastBlock()["index"] + 1;
+  }
+  hash(block) {
+    const blockString = JSON.stringify(block);
+    const hash = crypto
+      .createHmac(process.env.HASH_TYPE, process.env.CRYPTO_SECRET)
+      .update(blockString)
+      .digest("hex");
 
-     newTransaction  ()
+    return hash;
+  }
+  lastBlock() {
+    return this.chain.slice(-1)[0];
+  }
+}
 
-     hash (block) 
-
-     lastBlock ()
- } 
-
- module.exports = Blockchain
+module.exports = Blockchain;
